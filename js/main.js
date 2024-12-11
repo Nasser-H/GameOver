c5856f5109a1c0673f352e450e2da4f64042d888
Nasser-H <nasser.h0404@gmail.com>
2024-12-11 17:49:20 +0200

    secound commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const id5 = document.getElementById("id5");
+const Games = document.getElementById("Games");
+const DetailsGame = document.getElementById("DetailsGame");
+const closeDetailsGame = document.getElementById("closeDetailsGame");
+
+id5.addEventListener("click", function () {
+  Games.classList.add("d-none");
+  DetailsGame.classList.remove("d-none");
+});
+closeDetailsGame.addEventListener("click", function () {
+  DetailsGame.classList.add("d-none");
+  Games.classList.remove("d-none");
+});
